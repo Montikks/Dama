4d7b77db76bf4f65f770ffe0d965d6cbea41ffaf
PavelMrazek <pavelmrazek60@gmail.com>
2024-08-07 21:04:18 +0200

    Initial commit with current progress

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+from constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
+from game import Game
+from minimax import minimax
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Dáma')
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+        clock.tick(60)
+
+        if game.turn == WHITE:
+            value, new_board = minimax(game.get_board(), 3, True, game)
+            game.ai_move(new_board)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
